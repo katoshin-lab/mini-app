66b963cee46d177632e682fd4f0e91880a9b8f46
加藤慎也 <katoshinya@katoushinyanoMacBook-Air.local>
2019-10-12 21:26:03 +0900

    Edit-review-form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new
+    Review.create(review_params)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:text).merge(user_id: current_user.id, game_id: params[:game_id])
+  end
 end
